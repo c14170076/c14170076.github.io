7fec8f50682e70647accc6dc306906d002b3b5f2
c14170076 <66548263+c14170076@users.noreply.github.com>
2020-06-10 18:44:00 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,12 @@
+var CACHE_STATIC_NAME = 'cache';
+
+
 self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('first-app')
+    caches.open(CACHE_STATIC_NAME)
       .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
         cache.addAll([
           '/',
           '/index.html',
@@ -15,11 +20,27 @@ self.addEventListener('install', function(event) {
           '/src/images/icons/app-icon-512x512.png',
           '/src/css/app.css',
           '/src/js/app.js'
-        ])
+        ]);
+      })
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
       })
   );
   return self.clients.claim();
 });
+
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
