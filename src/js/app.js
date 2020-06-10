999ab8c7b65c925294d1db8ebe3bcfd2dc59f5ee
c14170076 <66548263+c14170076@users.noreply.github.com>
2020-06-10 19:08:16 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -54,13 +54,16 @@ button.addEventListener('click', function() {
   animate();
 });
 
-var deferredPrompt;
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
-  event.preventDefault();
-  deferredPrompt = event;
-  return false;
-});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
 
 var deferredPrompt;
 window.addEventListener('beforeinstallprompt', function(event) {
